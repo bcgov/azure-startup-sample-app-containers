9127a364af374cf4b91e745ba490bbb597e5c251
Adin Ermie <Adin.Ermie@gov.bc.ca>
2024-07-11 15:42:51 -0400

    Added multi-stage to deployment

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -7,10 +7,4 @@ locals {
 
   # Naming prefix for resources (ie. abc123-dev-lab)
   name_prefix = "${var.service_name}${local.location_sn}${var.env}"
-
-
-  #client_id       = var.client_id != "" ? var.client_id : env.ARM_CLIENT_ID
-  #client_secret   = var.client_secret != "" ? var.client_secret : env.ARM_CLIENT_SECRET
-  #tenant_id       = var.tenant_id != "" ? var.tenant_id : env.ARM_TENANT_ID
-  #subscription_id = var.subscription_id != "" ? var.subscription_id : env.ARM_SUBSCRIPTION_ID
 }
